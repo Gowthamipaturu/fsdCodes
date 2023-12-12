5185fc3af55bada5e2807f1806a2bffa7a109fa6
Gowthamipaturu <gowthamilakshmipathy.k@gmil.com>
2023-12-12 21:46:09 +0530

    adding operators file

---
+++ operators.js
@@ -0,0 +1,92 @@
+// console.log(GowthamiLakshmipathy)
+let x=5;
+let y=4;
+let z=x+y;
+console.log(z);
+// Different way
+let a,b,c;
+a=8;
+b=8;
+c=a-b;
+console.log(c);
+// String
+let name = "Gowthami";
+console.log(name);
+// To check datatype
+console.log(typeof a);
+console.log( typeof name);
+// Arithmetic Operations
+let n1=2;
+let n2=7;
+let n3=n1*n2;  //Multiplication
+console.log(n3);
+let p=16;
+let q=4;
+let r=p/q; // Division
+console.log(r);
+let num1=6;
+let out=num1**3;  //Exponent
+console.log(out);
+console.log(num1%5);  //Division Remainder or Modulus
+//Assignment Operators
+let a1=4;
+a1=5; //output will be 5, as we reassigning value to a1
+a1=a1+6; //regular addition assignment operation
+console.log("a1 value:", a1);
+a1=a1-4; //subtraction
+console.log("a1 value:", a1);
+//Shorthand
+let b1=8;
+b1+=5;
+console.log("b1 value:", b1);
+b1-=3;
+console.log("b1 value:", b1);
+b1*=6;
+console.log("b1 value:", b1);
+b1/=2;
+console.log("b1 value:", b1);
+b1**=8;
+console.log("b1 value:", b1);
+b1%=3;
+console.log("b1 value:", b1); //whatever in the double quotes will dispay as it is. 
+
+//String Operations
+let firstName="Gowthami";
+let lastName="Lakshmipathy";
+let fullName=firstName+lastName;
+fullName=firstName+","+lastName; //with "," between two strings
+fullName=firstName+" "+lastName; //For Space
+console.log("fullName:",fullName);
+
+let k=9;
+let l=7;
+console.log(k+l);  //2nos. operation
+let k1="9";
+let l1="7";
+console.log(k1+l1); //2strings
+let k2="9";
+let l2=7;
+console.log(k2+l2); //string with no. = Always String only
+let x1=9;
+let x2="7";
+let x3=8;  //9+"7"+8="978"
+console.log(x1+x2+x3); //string with no. = Always String only
+
+let y1=9;
+let y2=7;
+let y3="8";                     //9+7=16+"8"="168"
+console.log(y1+y2+y3);
+
+let z1="9";
+let z2=7;
+let z3=8;
+console.log(z1+z2+z3);  //"9"+7="97"+8="978"
+
+
+
+
+
+
+
+
+
