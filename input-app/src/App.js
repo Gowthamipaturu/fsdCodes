33e058918a2857d495fc29c2c6bac1a3199c7ca8
Gowthamipaturu <gowthamilakshmipathy.k@gmil.com>
2024-03-04 11:23:52 +0530

    adding

--- input-app/src/App.js
+++ input-app/src/App.js
@@ -1,8 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-// import { useState } from 'react';
+import { useState } from 'react';
+import KidDetailsModal from './KidDetailsModal';
 
 function App() {
+
+  const [showModal, setShowModal] = useState(false);
+  const [kidsData, setKidsData] = useState({ 
+
+  });
+
+  const [selectedRowData, setSelectedRowData] = useState("");
+
+  const handleCloseModal = () => setShowModal(false);
+  // const handleShowModal = () => setShowModal(true);
+  const handleShowModal = (data) =>{
+    setShowModal(true);
+    console.log("data", data);
+    setSelectedRowData(data);
+  } 
+
 
   // const [kidsData, setKidsData]= useState([]);
   // const [modalInfo, setModalInfo]= useState([]);
@@ -97,57 +114,41 @@ function App() {
   // const toggleTrueFalse=()=>{
   //   setShowModal(handleShow);
   // };
-  
+  console.log("showModal",showModal);
   return (
     <>
+    <button onClick={handleCloseModal}>Close</button>
+    {!showModal?<>
     <h1>Kids Details</h1>
+  
     <table class="table">
     <thead>
     <tr>
       <th scope="col">Sno</th>
       <th scope="col">Name</th>
       <th scope="col">Age</th>
+      <th scope="col">Action</th>
       {/* <th scope="col">Mother</th>
       <th scope="col">Father</th>
       <th scope="col">Bloodgroup</th> */}
     </tr>
   </thead>
   <tbody>
-    {KidsData.map((row, item)=>(
-      <tr>
-        <td>{item+1}</td>
+    {KidsData.map((row)=>(
+      <tr key={row.sno}>
+        <td>{row.sno}</td>
       <td>{row.name}</td>
       <td>{row.age}</td>
-      
-{/* <!-- Button trigger modal --> */}
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  More details
-</button>
-
-{/* <!-- Modal --> */}
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Kids Data</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      {/* <h3>{KidsData[item]}</h3> */}
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
+      <td><button onClick={()=>{handleShowModal(row)}}>Show More Details</button></td>
+    
 </tr>
+
       )  
 )}
 </tbody>
-  {/* rowEvents={rowEvents}
-  {show?{kidsData}:null} */}
   </table>
+  </>:<KidDetailsModal selectedRowData={selectedRowData}/>}
+{/* {showModal?<KidDetailsModal/>:null} */}
 
 </>
 
